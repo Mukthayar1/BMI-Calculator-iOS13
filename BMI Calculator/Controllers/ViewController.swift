73ba8d18dbd8eee009df8c7402d0f49c905b50f3
arafayklodhi <64356624+Mukthayar1@users.noreply.github.com>
2024-05-29 20:21:17 +0500

    working

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        HeightValue.text = "0";
+        WeightValue.text = "0"
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var HeightValue: UILabel!
+    @IBOutlet weak var WeightValue: UILabel!
+    
+    @IBAction func HeightSliderChange(_ sender: UISlider) {
+        print(String(format: "%.0f",sender.value))
+        HeightValue.text = String(format: "%.2f",sender.value)
+    }
+    
+    @IBAction func WeightSliderChange(_ sender: UISlider) {
+        WeightValue.text = String(format: "%.2f",sender.value)
 
+    }
 }
 
