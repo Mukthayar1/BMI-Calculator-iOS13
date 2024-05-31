9be23691083e03ebebaaffc305585131ba835dd9
arafayklodhi <64356624+Mukthayar1@users.noreply.github.com>
2024-05-31 14:24:09 +0500

    adding new class

---
+++ BMI Calculator/Controllers/SecondVewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondVewController.swift
+//  BMI Calculator
+//
+//  Created by Muhammad Mukhtayar on 31/05/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondVewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+}
